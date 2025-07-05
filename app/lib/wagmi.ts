81954f82e4a03eb6af07bca15aa2b3b8c6c6f4dd
jorvixsky <jorvixsky@proton.me>
2025-07-05 09:05:09 +0200

    update chains

--- app/lib/wagmi.ts
+++ app/lib/wagmi.ts
@@ -1,19 +1,8 @@
-
-import {
-  getDefaultConfig,
-  RainbowKitProvider,
-} from '@rainbow-me/rainbowkit';
-import { WagmiProvider } from 'wagmi';
-import {
-  mainnet,
-  polygon,
-  optimism,
-  arbitrum,
-  base,
-} from 'wagmi/chains';
+import { getDefaultConfig } from '@rainbow-me/rainbowkit';
+import { mainnet, arbitrum, optimism, polygon, base } from 'wagmi/chains';
 
 export const wagmiConfig = getDefaultConfig({
-  chains: [mainnet, polygon, optimism, arbitrum, base],
-  projectId: "2285a6530a6489b50a90620b88905aac",
-  appName: 'Token Aggregator',
-});
+    chains: [mainnet, polygon, optimism, arbitrum, base],
+    projectId: "2285a6530a6489b50a90620b88905aac",
+    appName: 'Token Aggregator',
+}); 
